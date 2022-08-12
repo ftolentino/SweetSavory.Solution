499322db986f40a73f4614b14ec599c9e7561a89
ftolentino <filmer.tolentino@gmail.com>
2022-08-12 16:22:25 -0700

    Update Controller methods

--- SweetSavory/Controllers/TreatsController.cs
+++ SweetSavory/Controllers/TreatsController.cs
@@ -65,45 +65,46 @@ namespace SweetSavory.Controllers
       return RedirectToAction("Index");
     }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   var thisTag = _db.Tags.FirstOrDefault(tag => tag.TagId == id);
-    //   return View(thisTag);
-    // }
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   var thisTag = _db.Tags.FirstOrDefault(tag => tag.TagId == id);
-    //   _db.Tags.Remove(thisTag);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
-    // public ActionResult AddRecipe(int id)
-    // {
-    //   var thisTag = _db.Tags.FirstOrDefault(tag => tag.TagId == id);
-    //   ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
-    //   return View(thisTag);
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // [HttpPost]
-    // public ActionResult AddRecipe(Tag tag, int RecipeId)
-    // {
-    //   if (RecipeId !=0)
-    //   {
-    //     _db.TagRecipe.Add(new TagRecipe() { RecipeId = RecipeId, TagId = tag.TagId }); 
-    //     _db.SaveChanges();
-    //   }
-    //   return RedirectToAction("Index");
-    // }
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
 
-    // [HttpPost]
-    // public ActionResult DeleteRecipe(int joinId)
-    // {
-    //   var joinEntry = _db.TagRecipe.FirstOrDefault(entry => entry.TagRecipeId == joinId);
-    //   _db.TagRecipe.Remove(joinEntry);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId !=0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId }); 
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }

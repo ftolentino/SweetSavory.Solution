4529afeb47450c46c90f78745b8d7614087a6c8a
ftolentino <filmer.tolentino@gmail.com>
2022-08-12 15:36:46 -0700

    Add constructor for jointable

--- SweetSavory/Models/Treat.cs
+++ SweetSavory/Models/Treat.cs
@@ -4,13 +4,13 @@ namespace SweetSavory.Models
 {
     public class Treat
     {
-        // public Treat()
-        // {
-        //     this.JoinEntities = new HashSet<FlavorTreat>();
-        // }
+        public Treat()
+        {
+            this.JoinEntities = new HashSet<FlavorTreat>();
+        }
 
         public int TreatId { get; set; }
         public string Name { get; set; }
-        // public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+        public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
     }
 }
